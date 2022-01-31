e0537bc63b0fd83c7b8a92026074ce0cdfe92c49
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-01-31 21:22:57 +0100

    Update Rocket.java
    Implemented abstract methods

--- src/main/Rocket.java
+++ src/main/Rocket.java
@@ -1,12 +1,23 @@
 package main;
 
 public class Rocket extends BaseObject{
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
     }
 
+    /**
+     * Draw the object on the canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Move the object down by one step.
+     */
     @Override
     public void move() {
-
+        y--;
     }
 }
