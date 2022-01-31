3bab5f19a8dc495648ce2fdcdd554fbbb326a3a0
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-01-31 18:42:25 +0100

    Parts 3 -4
    Space:
    Added several fields, getters, constructor

--- src/main/Space.java
+++ src/main/Space.java
@@ -1,6 +1,65 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Space {
-    public static void main(String[] args) {
+        private int width;
+        private int height;
+        private Spaceship ship;
+        private List<Ufo> ufos = new ArrayList<>();
+        private List<Rocket> rockets = new ArrayList<>();
+        private List<Bomb> bombs = new ArrayList<>();
+
+        public void setShip(Spaceship ship) {
+        this.ship = ship;
+    }
+
+        public int getWidth() {
+        return width;
+    }
+
+        public int getHeight() {
+        return height;
+    }
+
+        public Spaceship getShip() {
+        return ship;
+    }
+
+        public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+        public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+        public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+        public static void main(String[] args) {
+
+    }
+
+    public void run(){
+
+    }
+
+    /**
+     * This method is responsible for drawing game field "frame"
+     */
+    public void draw(){
+
+    }
+
+    public void sleep (int ms){
+
     }
 }
