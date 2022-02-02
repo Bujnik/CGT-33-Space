a9dec90ff37947c40f59ec30f4699dfd18e07a3c
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-02-02 13:01:50 +0100

    Update Ufo.java
    Ufo class is implemented similar to Spaceship

--- src/main/Ufo.java
+++ src/main/Ufo.java
@@ -1,12 +1,45 @@
 package main;
 
 public class Ufo extends BaseObject{
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    // Picture for drawing
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
     }
 
+    /**
+     * This method draws the object on the canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    /**
+     * Move the object by one step in a random direction.
+     */
     @Override
     public void move() {
+        x += Math.random() * 2 - 1;;
+        y += Math.random() * 2 - 1;;
 
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2d - 1);
+
+        if (Math.random() > 0.9) fire();
+    }
+
+    /**
+     * We're shooting.
+     * Drop (create) one bomb directly below.
+     */
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + radius));
     }
 }
